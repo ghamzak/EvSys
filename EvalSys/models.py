9cb9851f85d43e403d4ed060a7e580f3f30cc62c
ghamzak <ghka9436@colorado.edu>
2018-03-01 19:49:41 -0700

    Delete Model commit

--- EvalSys/models.py
+++ EvalSys/models.py
@@ -1,19 +1,23 @@
-from __future__ import unicode_literals
-
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-class Qualia(models.Model):
-	node = models.CharField(max_length=50)
-	qualevalue = models.CharField(max_length=500)
-	post = models.BooleanField(default=False)
-	qualename = models.CharField(max_length=20, default='Telic')
-	sb_annotations = models.BooleanField(default=False)
-	cb_annotations = models.BooleanField(default=False)
-	# gk_annotations = models.BooleanField(default=False)
+### No Models!
 
 
-	def __str__(self):
-		return self.node
+
+# from __future__ import unicode_literals
+
+# from django.db import models
+# from django.contrib.auth.models import User
+# # Create your models here.
+# class Qualia(models.Model):
+# 	node = models.CharField(max_length=50)
+# 	qualevalue = models.CharField(max_length=500)
+# 	post = models.BooleanField(default=False)
+# 	qualename = models.CharField(max_length=20, default='Telic')
+# 	sb_annotations = models.BooleanField(default=False)
+# 	cb_annotations = models.BooleanField(default=False)
+# 	# gk_annotations = models.BooleanField(default=False)
+
+
+# 	def __str__(self):
+# 		return self.node
 
 
